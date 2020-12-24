4bb11b368edcf679cecf72c4b09d977d515db208
Matt Brown <matthewbrown0798@gmail.com>
2020-12-24 12:11:46 -0500

    added backend handling for adding new user to a room

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const router = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const { callbackify } = require("util");
 
 const app = express();
@@ -17,7 +18,19 @@ io.on("connection", (socket) => {
   console.log("We have a new connection!");
 
   socket.on("join", ({ name, room }, callback) => {
-    console.log({ name, room });
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the ${user.room} room`,
+    });
+    //sends message to everyone but the new user
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined` });
+    socket.join(user.room);
+    callback();
   });
   socket.on("disconnect", () => {
     console.log("User has left");
