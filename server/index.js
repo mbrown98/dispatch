1265017f8869591ff49f841b84ce883330133a28
Matt Brown <matthewbrown0798@gmail.com>
2020-12-24 10:44:30 -0500

    added server side socket connect and disconnect functionality

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+  console.log("We have a new connection!");
+  socket.on("disconnect", () => {
+    console.log("User has left");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
